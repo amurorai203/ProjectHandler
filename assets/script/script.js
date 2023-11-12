d8a8f436fcf95773002754414836b8b0226a8889
Andy Lai <laihochi@gmail.com>
2023-11-12 10:24:26 +0000

    amended to store project item when click in saveBtn

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,9 +7,10 @@ var slotElt = $(".container");
 var today = dayjs();
 var currentHour = today.format("HH");
 var savedData = [];
-var loadData = JSON.parse(localStorage.getItem("Chellenge 7 data"));
+const dataStore = "Chellenge 7 data";
+var loadData = JSON.parse(localStorage.getItem(dataStore));
 
-console.log(loadData);
+// console.log(loadData);
 
 const slotClass = {
     slotTime: "00:00",
@@ -20,7 +21,6 @@ const slotClass = {
 currentDayElt.text(today.format("dddd, MMMM D[th]"));
 
 function rendorSlot(descText, descItem, mode){
-
     var divElt = $("<div>");
     divElt.addClass("input-group mb-3");
     var spanElt = $("<span>");
@@ -51,14 +51,12 @@ function rendorSlot(descText, descItem, mode){
 }
 
 function loadExistSlot(slot){
-    // console.log("Look for: " + slot);
     var returnString = "";
     if (loadData != null){
         var objLocated = loadData.find(({slotTime}) => slotTime  === slot);
         returnString = objLocated.slotItem;
     }
 
-    // console.log(objLocated.slotItem);
     return returnString;
 }
 
@@ -75,8 +73,6 @@ function buildSlot(){
         var objItem = Object.create(slotClass);
         objItem.slotTime = itemElt;
         objItem.slotItem = loadExistSlot(itemElt);
-        // objItem.slotItem = "Handle Challenge " + nextHour;
-        // console.log(objItem.slotItem);
         sectionSlot.push(objItem);
 
         if (nextHour === currentHour){
@@ -88,15 +84,16 @@ function buildSlot(){
         }
     } while (nextDatetime < endDatetime)
 
-    // console.log(sectionSlot);
 }
 
 buildSlot();
-// localStorage.setItem("Chellenge 7 data", JSON.stringify(sectionSlot));
 
 $("img").on("click", function(event){
     var element = event.target;
     var objKey = element.parentElement.children[0].textContent;
     var objValue = element.parentElement.children[1].value;
-    console.log(objKey + ":" + objValue);
+    // console.log(objKey + ":" + objValue);
+
+    (sectionSlot.find(({slotTime}) => slotTime  === objKey)).slotItem = objValue;
+    localStorage.setItem(dataStore, JSON.stringify(sectionSlot));
 })
